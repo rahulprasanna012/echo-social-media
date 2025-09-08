16616ff03196b7ba4bf2b58f3a6e5b7b9be6dcaf
rahulprasanna012 <rahulprasanna012@gmail.com>
2025-09-09 03:48:06 +0530

    post model

---
+++ server/src/models/post.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true, 
+      trim: true,
+    },
+    image: {
+      type: String, 
+    },
+    label: [
+      {
+        type: String, 
+        trim: true,
+      },
+    ],
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User", 
+      required: true,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        text: { type: String, required: true },
+        createdAt: { type: Date, default: Date.now },
+      },
+    ],
+  },
+  { timestamps: true } 
+);
+
+export default mongoose.model("Post", postSchema);
