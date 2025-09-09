b1d2091109e47ffe971f6c06e2249e778832a1d9
rahulprasanna012 <rahulprasanna012@gmail.com>
2025-09-09 23:45:33 +0530

    done

--- server/src/server.js
+++ server/src/server.js
@@ -13,13 +13,14 @@ config()
 const app = express()
 const allowedOrigins = [process.env.ORIGIN];
 
-app.use(
-  cors({
-    origin: allowedOrigins,
-    credentials: true,
-  })
-);
-console.log(process.env.ORIGIN);
+const ALLOWED_ORIGIN = "https://social.prasannanxtwave.site";
+
+app.use(cors({
+  origin: ALLOWED_ORIGIN,   
+  credentials: true,         
+  methods: ["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
